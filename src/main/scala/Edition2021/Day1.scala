13c44b3299b5ae03ccec8eec4e0d080ea1701fe6
Pierre Verbe <pierre.verbe@free.fr>
2021-11-30 17:29:48 +0100

    Updated Day1

--- src/main/scala/Edition2021/Day1.scala
+++ src/main/scala/Edition2021/Day1.scala
@@ -3,10 +3,16 @@ package Edition2021
 import org.apache.spark.SparkContext
 
 object Day1 {
+  def parseLine(line: String): (Int, Double) = {
+    val fields = line.split(",")
+    (0, 0.0)
+  }
+
   def main(args: Array[String]) {
 
     val sc = new SparkContext("local[*]", "AdventOfCode Day1")
     val input = sc.textFile("")
+    val mappedInput = input.map(parseLine)
 
   }
 }
