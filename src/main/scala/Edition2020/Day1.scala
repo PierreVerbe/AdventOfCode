e6cfad972eb3c78842fdd69c1d49d2122a9ac7a5
Pierre Verbe <pierre.verbe@free.fr>
2021-11-30 16:48:02 +0100

    Added input files for aoc2020 and base files

---
+++ src/main/scala/Edition2020/Day1.scala
@@ -0,0 +1,7 @@
+package Edition2020
+
+object Day1 extends App{
+  // Answer 1: 482811
+
+  // Answer 2: 193171814
+}
---
+++ src/main/scala/Edition2020/Day2.scala
@@ -0,0 +1,7 @@
+package Edition2020
+
+object Day2 extends App{
+  // Answer 1: 517
+
+  // Answer 2: 284
+}
