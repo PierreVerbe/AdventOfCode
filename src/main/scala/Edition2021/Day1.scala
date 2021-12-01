77d71a3840616715b8acc652c255db363101b39e
Pierre Verbe <pierre.verbe@socgen.com>
2021-12-01 16:02:57 +0100

    Day1 done

--- src/main/scala/Edition2021/Day1.scala
+++ src/main/scala/Edition2021/Day1.scala
@@ -1,18 +1,37 @@
 package Edition2021
 
-import org.apache.spark.SparkContext
+import scala.io.Source
 
 object Day1 {
-  def parseLine(line: String): (Int, Double) = {
-    val fields = line.split(",")
-    (0, 0.0)
+  def enigma1(input: List[Int]): Int = {
+    val slidingWindow = input.sliding(2, 1).toList
+
+    val measurements = slidingWindow.map(listItem => {
+      if (listItem.head < listItem(1)) "increased"
+      else if (listItem.head == listItem(1)) "no change"
+      else "decreased"
+    })
+    measurements.count(_ == "increased")
+  }
+
+  def enigma2(input: List[Int]): Int = {
+    val slidingWindow = input.sliding(3, 1).toList.map(_.sum)
+    enigma1(slidingWindow)
   }
 
   def main(args: Array[String]) {
+    val filename = "src/main/resources/Edition2021/Day1.txt"
 
-    val sc = new SparkContext("local[*]", "AdventOfCode Day1")
-    val input = sc.textFile("")
-    val mappedInput = input.map(parseLine)
+    val source = Source.fromFile(filename)
+    val lineList = source.getLines.toList.map(_.toInt)
+    source.close()
 
+    // Enigma 1
+    val result1 = enigma1(lineList)
+    println(s"$result1")
+
+    // Enigma 2
+    val result2 = enigma2(lineList)
+    println(s"$result2")
   }
 }
